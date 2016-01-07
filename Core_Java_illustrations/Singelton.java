0f5ed3d891d56d183afe3e85a40558274341b0c4
Samet Keserci <sammath@users.noreply.github.com>
2016-01-07 18:16:50 -0500

    Update Singelton.java

--- Core_Java_illustrations/Singelton.java
+++ Core_Java_illustrations/Singelton.java
@@ -1,1 +1,32 @@
 
+package javaprep;
+
+public class Singleton {
+
+		
+	 static Singleton instance = null;
+	
+	//constructor
+	 protected Singleton() {
+		   System.out.println("created");
+	 }
+	   //to make thread safe synchronized added
+	 public static synchronized Singleton getInstance() {
+	      if(instance == null) {
+	         instance = new Singleton();
+	         //System.out.println("created");
+	      }
+	      return instance;
+	   }
+	   
+	   public static void main(String[] args){
+	     
+		   getInstance();
+		   getInstance();
+		   getInstance();
+		   getInstance();
+		   getInstance();
+		   getInstance();
+		   				   
+	   }
+}
