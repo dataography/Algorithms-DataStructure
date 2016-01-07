e4d73543755544c394675114ca58c853a176101c
Samet Keserci <sammath@users.noreply.github.com>
2016-01-07 18:12:14 -0500

    Update Timer.java

--- Core_Java_illustrations/Timer.java
+++ Core_Java_illustrations/Timer.java
@@ -1,1 +1,44 @@
 
+package javaprep;
+
+
+public class timer {
+	
+	private static timer tObj = null;
+	static int counttimer = 0;
+	static int countgettimer = 0;
+	//constructor
+	public timer(){
+		System.out.println("counttimer constructor = " + ++counttimer);
+	}
+
+	public void aragazi(){
+	System.out.print("aragazi");
+	}
+
+	public static synchronized timer getTimerObject () {
+		
+		if (tObj == null){
+			tObj = new timer();
+		System.out.println("count gettimer created=" + ++countgettimer);
+		
+		}else{
+			System.out.println("already created yoo");
+		}
+		return tObj;
+	}
+	 
+	public static void main(String[] args) {
+		
+		timer t = new timer();
+		t.getTimerObject();
+		t.getTimerObject();
+		t.getTimerObject();
+		t.getTimerObject();
+		t.getTimerObject();
+		t.getTimerObject();
+		
+		
+	}
+
+}
